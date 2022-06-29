1a7466f95a62234e715f6f3981b07c9098f4855c
mitchellri <mitchell.rivett@hotmail.com>
2022-06-29 00:28:41 -0700

    Basic app.js modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        react-redux-guide
+      </h1>
+      <ul>
+        <li>React, Redux & Firebase App Tutorial: <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3">https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3</a></li>
+        <li>Complete React Tutorial (with Redux): <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG">https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG</a></li>
+      </ul>
+      <ol>
+        <li>
+          <h2>
+            Getting started
+          </h2>
+          <ol>
+            <li>Install npm: <a href="https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac">https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac</a></li>
+            <li>Create website template: <code>npx create-react-app my-app</code></li>
+          </ol>
+        </li>
+      </ol>
     </div>
   );
 }
