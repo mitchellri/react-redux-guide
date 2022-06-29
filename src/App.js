57b831984160cdbb019d11fc36679f4736182ef7
mitchellri <mitchell.rivett@hotmail.com>
2022-06-29 01:13:40 -0700

    Add Materialize

--- src/App.js
+++ src/App.js
@@ -4,21 +4,42 @@ function App() {
       <h1>
         react-redux-guide
       </h1>
-      <ul>
-        <li>React, Redux & Firebase App Tutorial: <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3">https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3</a></li>
-        <li>Complete React Tutorial (with Redux): <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG">https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG</a></li>
-      </ul>
+      <span>Created by: <a href="https://github.com/mitchellri">mitchellri</a></span>
+
+      <h2>Getting started</h2>
       <ol>
         <li>
-          <h2>
-            Getting started
-          </h2>
+          <a href="https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac">Install npm</a>
+        </li>
+        <li>
+          Create website template (<a href="https://youtu.be/5QwNCX3UbXc?t=105">Complete React Tutorial (& Redux) #10 - Create React App</a>)
           <ol>
-            <li>Install npm: <a href="https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac">https://radixweb.com/blog/installing-npm-and-nodejs-on-windows-and-mac</a></li>
-            <li>Create website template: <code>npx create-react-app my-app</code></li>
+            <li>
+              <code>npx create-react-app my-app</code>
+            </li>
+            <li>
+              In the new project folder: <code>npm start</code>
+            </li>
           </ol>
         </li>
       </ol>
+
+      <h2>Adding tools</h2>
+      <ol>
+        <li>
+          <a href="https://materializecss.com/getting-started.html">Add preset style classes</a> (<a href="https://youtu.be/r5b0spRlnlU?t=123">React, Redux & Firebase App Tutorial #3 - Setting Up</a>)
+        </li>
+      </ol>
+
+      <h2>Resources</h2>
+      <ul>
+        <li>
+          <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9iWstfXntcj8f-dFZ4UtlN3">React, Redux & Firebase App Tutorial</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG">Complete React Tutorial (with Redux)</a>
+        </li>
+      </ul>
     </div>
   );
 }
